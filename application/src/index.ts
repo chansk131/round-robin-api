84454fec9328efb8e8e5c5cae0f64175806353d2
Theerapat Muangpoon <chansk131@gmail.com>
2024-08-19 03:47:42 +0700

    feat: add post endpoint and send back request body in response

--- application/src/index.ts
+++ application/src/index.ts
@@ -3,8 +3,14 @@ import express from "express"
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 app.get("/", (_, res) => {
   res.send("Hello World!")
+})
+
+app.post("/", (req, res) => {
+  res.send(req.body)
 })
 
 app.listen(port, () => {
