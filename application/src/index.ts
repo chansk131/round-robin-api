91d640210f143a49376fd3d07f7e464ff7143883
Theerapat Muangpoon <chansk131@gmail.com>
2024-08-19 05:41:53 +0700

    feat: add slow and error for application

--- application/src/index.ts
+++ application/src/index.ts
@@ -3,18 +3,38 @@ import express from "express"
 const app = express()
 const port = process.env.PORT || 3000
 
+let mode: "normal" | "slow" | "error" = "normal"
+
 app.use(express.json())
 
 app.get("/", (_, res) => {
   res.send("Hello World!")
 })
 
-app.get("/health-check", (_, res) => {
-  res.send("OK")
+app.get("/health-check", async (_, res) => {
+  switch (mode) {
+    case "normal":
+      res.send("OK")
+      break
+    case "slow":
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      res.send("OK")
+      break
+    case "error":
+      res.status(503)
+      break
+    default:
+      break
+  }
 })
 
 app.post("/", (req, res) => {
   res.send(req.body)
+})
+
+app.post("/debug", (req, res) => {
+  mode = req.body.mode
+  res.send({ mode })
 })
 
 app.listen(port, () => {
